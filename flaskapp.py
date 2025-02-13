f63dd2fd3b7146151a266f05adc9bced81a1b8b6
Oliver Souter <73543366+o-souter@users.noreply.github.com>
2025-02-13 10:29:39 +0000

    Tidied up code

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,5 @@
 import flask
 from flask import request
-import requests
-import PIL
-from io import BytesIO
-import base64
-from PIL import Image
 import os, shutil
 
 app = flask.Flask(__name__)
@@ -15,7 +10,6 @@ uploadDir = "upload"
 def handle_call():
     return "Successfully Connected"
 
-# @app.route("/")
 
 @app.route("/upload-image", methods=['POST'])
 def store_image():
@@ -26,53 +20,8 @@ def store_image():
     image.save(f"./upload/{image.filename}")
     print("Recieved and stored image from app capture, at: " + f"./upload/{image.filename}", flush=True)
     return {"message": "Image recieved and stored successfully", "filename":image.filename}
-    # # ----- SECTION 1 -----  
-    # #File naming process for nameless base64 data.
-    # #We are using the timestamp as a file_name.
-    # from datetime import datetime
-    # dateTimeObj = datetime.now()
-    # file_name_for_base64_data = dateTimeObj.strftime("%d-%b-%Y--(%H-%M-%S)")
     
-    # #File naming process for directory form <file_name.jpg> data.
-    # #We are taken the last 8 characters from the url string.
-    # file_name_for_regular_data = url[-10:-4]
-    # data = request.form
-    # fileName = data.get("name")
-    # print("Recieved the file: " + fileName)
-    # ----- SECTION 2 -----
-    # try:
-    #     # Base64 DATA
-    #     if "data:image/jpeg;base64," in url:
-    #         base_string = url.replace("data:image/jpeg;base64,", "")
-    #         decoded_img = base64.b64decode(base_string)
-    #         img = Image.open(BytesIO(decoded_img))
-
-    #         file_name = file_name_for_base64_data + ".jpg"
-    #         img.save(file_name, "jpeg")
-
-    #     # Base64 DATA
-    #     elif "data:image/png;base64," in url:
-    #         base_string = url.replace("data:image/png;base64,", "")
-    #         decoded_img = base64.b64decode(base_string)
-    #         img = Image.open(BytesIO(decoded_img))
-
-    #         file_name = file_name_for_base64_data + ".png"
-    #         img.save(file_name, "png")
-
-    #     # Regular URL Form DATA
-    #     else:
-    #         response = requests.get(url)
-    #         img = Image.open(BytesIO(response.content)).convert("RGB")
-    #         file_name = file_name_for_regular_data + ".jpg"
-    #         img.save(file_name, "jpeg")
-        
-    # # ----- SECTION 3 -----    
-    #     status = "Image has been succesfully sent to the server."
-    # except: #Exception as e:
-    #     status = "Error! = " + str(e)
-
-
-    return# status
+    return
 
 
 
